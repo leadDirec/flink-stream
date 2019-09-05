088a2dd706fe0477aa0802e71e746c002fd4024b
xiangdao <89612533@qq.com>
2019-09-05 10:44:22 +0800

    add properties

--- src/main/java/com/wallstcn/app/UserPortrait.java
+++ src/main/java/com/wallstcn/app/UserPortrait.java
@@ -1,4 +1,10 @@
 package com.wallstcn.app;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class UserPortrait {
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    }
 }
---
+++ src/main/java/com/wallstcn/util/Property.java
@@ -0,0 +1,73 @@
+package com.wallstcn.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+public class Property {
+
+    private static final String envKey = "CONFIGOR_ENV";
+
+    private static final Logger LOG = LoggerFactory.getLogger(Property.class);
+
+    private final static String CONF_NAME = ".config.properties";
+
+    private static Properties contextProperties;
+
+    static {
+        InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(Property.getEnvConf(""));
+        contextProperties = new Properties();
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(in, "UTF-8");
+            contextProperties.load(inputStreamReader);
+        } catch (IOException e) {
+            LOG.debug(">>>flink-2-hbase<<<资源文件加载失败!");
+            e.printStackTrace();
+        }
+        LOG.debug(">>>flink-2-hbase<<<资源文件加载成功");
+    }
+
+//    public static String getEnvConf(String jvmKey, String key) {
+//        String jvm = System.getProperty(jvmKey);
+//        String env = System.getProperty(key);
+//        return StringUtils.isNotEmpty(jvm) ? jvm : //
+//                (StringUtils.isNotEmpty(env) ? "classpath:config/spring/" + env
+//                        + "-env-conf.properties" : //
+//                        "classpath:config/spring/dev-env-conf.properties");
+//    }
+
+    public static String getEnvConf(String key) {
+        if (StringUtils.isEmpty(key)) {
+            key = envKey;
+        }
+        String env = System.getProperty(key);
+        if (StringUtils.isEmpty(env)) {
+            return "local".concat(CONF_NAME);
+        }
+        return env.concat(CONF_NAME);
+    }
+
+    public static String getValue(String key) {
+        return contextProperties.getProperty(key);
+    }
+
+    public static int getIntValue(String key) {
+        String strValue = getValue(key);
+        // 注意，此处没有做校验，暂且认为不会出错
+        return Integer.parseInt(strValue);
+    }
+
+    public static Properties getKafkaProperties(String groupId) {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", getValue("kafka.bootstrap.servers"));
+        properties.setProperty("zookeeper.connect", getValue("kafka.zookeeper.connect"));
+        properties.setProperty("group.id", groupId);
+        return properties;
+    }
+
+}
