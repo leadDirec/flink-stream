e359c5f6632a714029e258ed3a77f54388cf266c
xiangdao <89612533@qq.com>
2019-09-06 18:51:28 +0800

    add

--- src/main/java/com/wallstcn/transformation/LogEntitySlidingEventTimeWindow.java
+++ src/main/java/com/wallstcn/transformation/LogEntitySlidingEventTimeWindow.java
@@ -9,7 +9,10 @@ import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.triggers.EventTimeTrigger;
 import org.apache.flink.streaming.api.windowing.triggers.Trigger;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @author xiangdao
@@ -21,12 +24,14 @@ public class LogEntitySlidingEventTimeWindow extends WindowAssigner<Object, Time
     private final long size;
     private final long slide;
     private final long offset;
+    private Integer count;
 
     protected LogEntitySlidingEventTimeWindow(long size, long slide, long offset) {
         if (offset >= 0L && offset < slide && size > 0L) {
             this.size = size;
             this.slide = slide;
             this.offset = offset;
+            this.count = 0;
         } else {
             throw new IllegalArgumentException("LogEntitySlidingEventTimeWindow parameters must satisfy 0 <= offset < slide and size > 0");
         }
@@ -34,7 +39,20 @@ public class LogEntitySlidingEventTimeWindow extends WindowAssigner<Object, Time
 
     @Override
     public Collection<TimeWindow> assignWindows(Object element, long timestamp, WindowAssignerContext context) {
-        return null;
+        if (timestamp > Long.MIN_VALUE) {
+            List<TimeWindow> windows = new ArrayList<>((int) (size<<1 / slide));
+            long lastStart = TimeWindow.getWindowStartWithOffset(timestamp, offset, slide);
+            for (long start = lastStart;
+                 start > timestamp - size;
+                 start -= slide) {
+                windows.add(new TimeWindow(start, start + size));
+            }
+            return windows;
+        } else {
+            throw new RuntimeException("Record has Long.MIN_VALUE timestamp (= no timestamp marker). " +
+                    "Is the time characteristic set to 'ProcessingTime', or did you forget to call " +
+                    "'DataStream.assignTimestampsAndWatermarks(...)'?");
+        }
     }
 
     @Override
