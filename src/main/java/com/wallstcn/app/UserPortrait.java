daa2a7b6795731b29ac467fa0810acd5581bb4ba
xiangdao <89612533@qq.com>
2019-09-06 13:22:26 +0800

    add

--- src/main/java/com/wallstcn/app/UserPortrait.java
+++ src/main/java/com/wallstcn/app/UserPortrait.java
@@ -23,10 +23,28 @@ public class UserPortrait {
     public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+//        // 每隔1000 ms进行启动一个检查点【设置checkpoint的周期】
+//        env.enableCheckpointing(1000);
+//// 高级选项：
+//// 设置模式为exactly-once （这是默认值）
+//        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+//// 确保检查点之间有至少500 ms的间隔【checkpoint最小间隔】
+//        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(500);
+//// 检查点必须在一分钟内完成，或者被丢弃【checkpoint的超时时间】
+//        env.getCheckpointConfig().setCheckpointTimeout(60000);
+//// 同一时间只允许进行一个检查点
+//        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+//// 表示一旦Flink处理程序被cancel后，会保留Checkpoint数据，以便根据实际需要恢复到指定的Checkpoint【详细解释见备注】
+//        env.getCheckpointConfig().enableExternalizedCheckpoints(ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+//        ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION:表示一旦Flink处理程序被cancel后，会保留Checkpoint数据，以便根据实际需要恢复到指定的Checkpoint
+//        ExternalizedCheckpointCleanup.DELETE_ON_CANCELLATION: 表示一旦Flink处理程序被cancel后，会删除Checkpoint数据，只有job执行失败的时候才会保存checkpoint
+
+
 //        env.setParallelism(1);
         env.enableCheckpointing(1000, CheckpointingMode.EXACTLY_ONCE);
 
-        env.setStateBackend(new RocksDBStateBackend(Property.getValue("state.checkpoints.dir"), true));
+        env.setStateBackend(new RocksDBStateBackend(Property.getValue("state.checkpoints.dir"), true).getCheckpointBackend());
 
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
@@ -46,7 +64,7 @@ public class UserPortrait {
                 .assignTimestampsAndWatermarks(new LogTimeStampPeriodExtractor()).setParallelism(parallelism)
                 .keyBy("userId")
                 .timeWindow(Time.days(6),Time.days(1))
-                .trigger(LogEntitytrigger.create()).sum(1);
+                .trigger(LogEntitytrigger.create());
 //                .evictor(new LogEntityEvictor())
 //                .apply();
         env.execute("User Portrait");
