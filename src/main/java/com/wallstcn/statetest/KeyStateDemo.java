8552541c116785d235bfaffe025a10ea4369c22f
xiangdao <89612533@qq.com>
2019-09-10 00:39:54 +0800

    add

---
+++ src/main/java/com/wallstcn/statetest/KeyStateDemo.java
@@ -0,0 +1,78 @@
+package com.wallstcn.statetest;
+
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.StateTtlConfig;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class KeyStateDemo  extends RichFlatMapFunction<Tuple2<Long,Long>,Tuple2<Long,Long>>{
+
+    private ValueState<Tuple2<Long,Long>> valueState;
+
+    @Override
+    public void flatMap(Tuple2<Long, Long> value, Collector<Tuple2<Long, Long>> collector) throws Exception {
+        Tuple2<Long,Long> currentValue = valueState.value();
+        if (currentValue == null) {
+            currentValue = Tuple2.of(0L,0L);
+        }
+        currentValue.f0 += 1;
+        currentValue.f1 += value.f1;
+        valueState.update(currentValue);
+        if (currentValue.f0 >= 3) {
+            collector.collect(Tuple2.of(value.f0,currentValue.f1/currentValue.f0));
+            valueState.clear();
+        }
+    }
+
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        //keyedState可以设置TTL过期时间
+        StateTtlConfig config = StateTtlConfig.newBuilder(Time.seconds(30)) //1
+                .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired) //2
+                .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite) //3
+                .build();
+        ValueStateDescriptor valueStateDescriptor = new ValueStateDescriptor("agvKeyedState", TypeInformation.of(new TypeHint<Tuple2<Long,Long>>() {}));
+        //设置支持TTL配置
+        valueStateDescriptor.enableTimeToLive(config);
+        valueState=getRuntimeContext().getState(valueStateDescriptor);
+    }
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env=StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        DataStream<Tuple2<Long,Long>> input=env.fromElements(
+                Tuple2.of(1L,4L),
+                Tuple2.of(1L,2L),
+                Tuple2.of(1L,6L),
+                Tuple2.of(2L,4L),
+                Tuple2.of(2L,4L),
+                Tuple2.of(3L,5L),
+                Tuple2.of(2L,3L),
+                Tuple2.of(1L,4L)
+        );
+
+        input.keyBy(0)
+                .flatMap(new KeyStateDemo())
+                .setParallelism(10)
+                .print();
+
+        env.execute();
+    }
+}
+
+//TTL过期时间   对于每一个keyed State，还可以设置TTL过期时间，它会将过期的state删除掉，通过下面的方式来设置TTL：
+// 1：ttl时长
+// 2： 定义更新 TTL 状态最后访问时间的更新类型：创建和读（StateTtlConfig.UpdateType.OnCreateAndWrite）、读和写（StateTtlConfig.UpdateType.OnReadAndWrite）
+// 3：定义状态的可见性，是否返回过期状态。启用 TTL：stateDescriptor.enableTimeToLive(retainOneDay);
---
+++ src/main/java/com/wallstcn/statetest/OperatorStateCheckpointedFucntion.java
@@ -0,0 +1,100 @@
+package com.wallstcn.statetest;
+
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Operator State
+//        Operator State是指在一个job中的一个task中的每一个operator对应着一个state，比如在一个job中，涉及到map，filter，sink等操作，
+//        那么在这些operator中，每一个可以对应着一个state（一个冰并行度），如果是多个并行度，那么每一个并行度都对应着一个state。
+//        对于Operator State主要有ListState可以进行使用
+//        可以看见上述案例中我们实现了它的initializeState、snapshotState等方法，
+//        如果实现了ListCheckpointed接口，就不需要我们自己初始化状态，
+//        直接从之前的状态进行恢复，只需要实现以下两个方法即可：
+//        List<T> snapshotState(long checkpointId, long timestamp) throws Exception;
+//        void restoreState(List<T> state) throws Exception;
+
+public class OperatorStateCheckpointedFucntion extends RichFlatMapFunction<Long, Tuple2<Integer,String>> implements CheckpointedFunction{
+
+    //托管状态
+    private ListState<Long> listState;
+    //原始状态
+    private List<Long> listElements;
+
+    @Override
+    public void flatMap(Long value, Collector<Tuple2<Integer, String>> collector) throws Exception {
+        if (value == 1) {
+            if (listElements.size() > 0 ) {
+                StringBuffer sb = new StringBuffer();
+                for (Long ele : listElements) {
+                    sb.append(ele+" ");
+                }
+                int sum = listElements.size();
+                collector.collect(new Tuple2<>(sum,sb.toString()));
+                listElements.clear();
+            }
+        }  else {
+            listElements.add(value);
+        }
+    }
+
+    /**
+     * 进行checkpoint进行快照
+     * @param context
+     * @throws Exception
+     */
+    @Override
+    public void snapshotState(FunctionSnapshotContext context) throws Exception {
+        listState.clear();
+        for(Long ele:listElements){
+            listState.add(ele);
+        }
+    }
+
+    /**
+     * state的初始状态，包括从故障恢复过来
+     * @param context
+     * @throws Exception
+     */
+    @Override
+    public void initializeState(FunctionInitializationContext context) throws Exception {
+        ListStateDescriptor listStateDescriptor=new ListStateDescriptor("checkPointedList",
+                TypeInformation.of(new TypeHint<Long>() {}));
+        listState = context.getOperatorStateStore().getListState(listStateDescriptor);
+        //如果是故障恢复
+        if (context.isRestored()) {
+            for (Long ele : listState.get()) {
+                listElements.add(ele);
+            }
+            listState.clear();
+        }
+    }
+
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        listElements=new ArrayList<Long>();
+    }
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env=StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        DataStream<Long> input=env.fromElements(1L,2L,3L,4L,7L,5L,1L,5L,4L,6L,1L,7L,8L,9L,1L);
+        input.flatMap(new OperatorStateCheckpointedFucntion()).setParallelism(1).print();
+        System.out.println(env.getExecutionPlan());
+        env.execute();
+    }
+}
