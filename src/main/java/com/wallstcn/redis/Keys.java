bbdf8074587985d8f0b106b2ae60337cc72dfb5a
xiangdao <89612533@qq.com>
2019-09-11 11:22:25 +0800

    add

--- src/main/java/com/wallstcn/redis/Keys.java
+++ src/main/java/com/wallstcn/redis/Keys.java
@@ -1,9 +1,9 @@
 package com.wallstcn.redis;
 
 public class Keys {
-    private static String UserActiveTimeLineKeys = "userportrait:timeline:%s"; //用户时间线 天（时间) list
+//    private static String UserActiveTimeLineKeys = "userportrait:timeline:%s"; //用户时间线 天（时间) list
     private static String UserArticleActionKeys = "userportrait:action:"; //用户行为 用户id 天（时间） 标签标识 hash
-    private static String UserLabelKeys = "userportrait:label:%d"; //用户标签 用户id hash
+//    private static String UserLabelKeys = "userportrait:label:%d"; //用户标签 用户id hash
     private static String UserLabelDatealKeys = "userportrait:labeldatail:"; //用户标签 用户id hash 需要每天开始重算
     private static String UserLabelActionScore = "userportrait:labelscore:"; //用户标签 用户id   存的是分数  需要每天开始重算
 
