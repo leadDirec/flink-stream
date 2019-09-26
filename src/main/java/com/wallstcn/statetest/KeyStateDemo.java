f007341ebe2172aff40e10e883d7a9f1b8203f12
xiangdao <89612533@qq.com>
2019-09-26 16:45:17 +0800

    兼容flink低版本

--- src/main/java/com/wallstcn/statetest/KeyStateDemo.java
+++ src/main/java/com/wallstcn/statetest/KeyStateDemo.java
@@ -1,12 +1,6 @@
 package com.wallstcn.statetest;
 
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
-import org.apache.flink.api.common.state.StateTtlConfig;
-import org.apache.flink.api.common.state.ValueState;
-import org.apache.flink.api.common.state.ValueStateDescriptor;
-import org.apache.flink.api.common.time.Time;
-import org.apache.flink.api.common.typeinfo.TypeHint;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.TimeCharacteristic;
@@ -14,48 +8,47 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
-import java.util.HashMap;
-import java.util.Map;
+//import org.apache.flink.api.common.state.StateTtlConfig;
 
 public class KeyStateDemo  extends RichFlatMapFunction<Tuple2<Long,Long>,Tuple2<Long,Long>>{
 
-    private Integer count = 0; //作用域为一个slot 跟operatorstate一样
-
-    private Map<Long,Integer> ha = new HashMap<>();
-
-    private ValueState<Tuple2<Long,Long>> valueState;
+//    private Integer count = 0; //作用域为一个slot 跟operatorstate一样
+//
+//    private Map<Long,Integer> ha = new HashMap<>();
+//
+//    private ValueState<Tuple2<Long,Long>> valueState;
 
     @Override
     public void flatMap(Tuple2<Long, Long> value, Collector<Tuple2<Long, Long>> collector) throws Exception {
-        count++;
-        Tuple2<Long,Long> currentValue = valueState.value();
-        if (currentValue == null) {
-            currentValue = Tuple2.of(0L,0L);
-        }
-        currentValue.f0 += 1;
-        currentValue.f1 += value.f1;
-        valueState.update(currentValue);
-        if (currentValue.f0 >= 3) {
-            collector.collect(Tuple2.of(value.f0,currentValue.f1/currentValue.f0));
-            valueState.clear();
-        }
-        ha.put(value.f0,0);
-        System.out.println(count);
+//        count++;
+//        Tuple2<Long,Long> currentValue = valueState.value();
+//        if (currentValue == null) {
+//            currentValue = Tuple2.of(0L,0L);
+//        }
+//        currentValue.f0 += 1;
+//        currentValue.f1 += value.f1;
+//        valueState.update(currentValue);
+//        if (currentValue.f0 >= 3) {
+//            collector.collect(Tuple2.of(value.f0,currentValue.f1/currentValue.f0));
+//            valueState.clear();
+//        }
+//        ha.put(value.f0,0);
+//        System.out.println(count);
     }
 
 
     @Override
     public void open(Configuration parameters) throws Exception {
-        super.open(parameters);
-        //keyedState可以设置TTL过期时间
-        StateTtlConfig config = StateTtlConfig.newBuilder(Time.seconds(30)) //1
-                .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired) //2
-                .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite) //3
-                .build();
-        ValueStateDescriptor valueStateDescriptor = new ValueStateDescriptor("agvKeyedState", TypeInformation.of(new TypeHint<Tuple2<Long,Long>>() {}));
-        //设置支持TTL配置
-        valueStateDescriptor.enableTimeToLive(config);
-        valueState=getRuntimeContext().getState(valueStateDescriptor);
+//        super.open(parameters);
+//        //keyedState可以设置TTL过期时间
+//        StateTtlConfig config = StateTtlConfig.newBuilder(Time.seconds(30)) //1
+//                .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired) //2
+//                .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite) //3
+//                .build();
+//        ValueStateDescriptor valueStateDescriptor = new ValueStateDescriptor("agvKeyedState", TypeInformation.of(new TypeHint<Tuple2<Long,Long>>() {}));
+//        //设置支持TTL配置
+//        valueStateDescriptor.enableTimeToLive(config);
+//        valueState=getRuntimeContext().getState(valueStateDescriptor);
     }
 
     public static void main(String[] args) throws Exception {
