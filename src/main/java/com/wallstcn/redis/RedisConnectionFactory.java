b8a92edb7afe87485b6025020c7901820d4f8176
xiangdao <89612533@qq.com>
2019-09-27 10:17:43 +0800

    add

--- src/main/java/com/wallstcn/redis/RedisConnectionFactory.java
+++ src/main/java/com/wallstcn/redis/RedisConnectionFactory.java
@@ -5,12 +5,16 @@ import com.wallstcn.util.connection.ConnectionFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.pool2.PooledObject;
 import org.apache.commons.pool2.impl.DefaultPooledObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 
 /**
  * @author xiangdao
  */
 public class RedisConnectionFactory   implements ConnectionFactory<Jedis> {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisConnectionFactory.class);
 
     private static final long serialVersionUID = 4024923894283696469L;
 
@@ -32,6 +36,7 @@ public class RedisConnectionFactory   implements ConnectionFactory<Jedis> {
      * <p>Description: 构造方法</p>
      */
     public RedisConnectionFactory(final String host, final int port, final int timeOut,final String auth) {
+        logger.error("RedisConnectionFactory:::"+host+">>>"+port+">>>>"+auth);
         this.jedisConfig = new JedisConfig();
         this.jedisConfig.setHost(host);
         this.jedisConfig.setPort(port);
