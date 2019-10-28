43b95f3b25c520ab09f151e409013a8c1d9a13b9
xiangdao <89612533@qq.com>
2019-10-28 11:04:55 +0800

    add

--- src/main/java/com/wallstcn/transformation/map/ArticleCoMapFuntion.java
+++ src/main/java/com/wallstcn/transformation/map/ArticleCoMapFuntion.java
@@ -3,12 +3,22 @@ package com.wallstcn.transformation.map;
 import com.wallstcn.models.LogEntity;
 import com.wallstcn.redis.Keys;
 import com.wallstcn.redis.RedisPool;
+import com.wallstcn.util.JacksonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ArticleCoMapFuntion extends BaseCoMapFuntion {
 
+    private static final Logger logger = LoggerFactory.getLogger(ArticleCoMapFuntion.class);
+
     @Override
     public Void map1(LogEntity logEntity) throws Exception {
+        logger.error("score ArticleCoMapFuntion >>>>>>>>>>>>"+JacksonUtils.toJson(getConfig()));
         Double score =  getConfig().get(logEntity.getAction());
+        if (score == 0) {
+            logger.error("score ArticleCoMapFuntion >>>>>>>>>>>> data  empty");
+            return null;
+        }
         for  (Integer label : logEntity.getRelatedLabels()) {
             String key = Keys.getUserLabelActionScore(logEntity.getUserId(),label);
             RedisPool.get().incrByFloat(key,score);
--- src/main/java/com/wallstcn/transformation/map/StockCoMapFuntion.java
+++ src/main/java/com/wallstcn/transformation/map/StockCoMapFuntion.java
@@ -3,14 +3,24 @@ package com.wallstcn.transformation.map;
 import com.wallstcn.models.LogEntity;
 import com.wallstcn.redis.Keys;
 import com.wallstcn.redis.RedisPool;
+import com.wallstcn.util.JacksonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StockCoMapFuntion extends BaseCoMapFuntion {
 
+    private static final Logger logger = LoggerFactory.getLogger(StockCoMapFuntion.class);
+
     @Override
     public Void map1(LogEntity logEntity) throws Exception {
+        logger.error("score StockCoMapFuntion >>>>>>>>>>>>"+ JacksonUtils.toJson(getConfig()));
         for (int i =0;i<logEntity.getRelatedLabels().length;i++) {
             Integer action = logEntity.getActions().get(i);
             Double score = getConfig().get(action);
+            if (score == 0) {
+                logger.error("score StockCoMapFuntion >>>>>>>>>>>> data  empty");
+                return null;
+            }
             String key = Keys.getUserLabelActionScore(logEntity.getUserId(),logEntity.getRelatedLabels()[i]);
             RedisPool.get().incrByFloat(key,score);
         }
